28bf0d67c3b10a12df40b97f3b8a5c82a490b3fb
王 胜 <wangsheng@cndoppler.cn>
2016-11-16 08:40:28 +0800

    增加内核编译脚本build.sh文件

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 使imx6的开发环境生效
+source /opt/yogurt/i.MX6-PD15.3.0/environment-setup-cortexa9hf-vfp-neon-phytec-linux-gnueabi
+
+# 指定编译器和配置文件的路径
+crossPath="/opt/yogurt/i.MX6-PD15.3.0/sysroots/x86_64-yogurtsdk-linux/usr/bin/arm-phytec-linux-gnueabi/arm-phytec-linux-gnueabi-"
+configPath="./configs/zImage.config"
+
+if [ ! -f "./.config" ]; then
+	cp $configPath ./.config
+	if [ -f "./.config" ]; then
+		make CROSS_COMPILE=$crossPath  ARCH=arm zImage
+	else 
+		echo "No .config file."
+	fi
+else
+	make CROSS_COMPILE=$crossPath  ARCH=arm zImage
+fi
+
